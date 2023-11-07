16f7afb9a7325ae5b6fce13356263b9413987702
nbena69 <126596584+nbena69@users.noreply.github.com>
2023-11-07 12:25:19 +0100

    RAS

--- src/Bibliothèque/Bibliotheque.java
+++ src/Bibliothèque/Bibliotheque.java
@@ -1,7 +1,17 @@
 package Bibliothèque;
 
+import metier.Lecteur;
+
+import java.util.Scanner;
+
 public class Bibliotheque {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Saisir votre identifiant : ");
+        String id = sc.nextLine();
+        System.out.println("Saisir votre mdp : ");
+        String mdp = sc.nextLine();
+        Lecteur lecteur = new Lecteur(id, mdp);
+        System.out.println("Bonjour : " + lecteur.getNom());
     }
 }
